c5bfc41102451dffb71e06f017903beb463c069e
Selynna Sun <ssun098@gmail.com>
2018-11-07 15:09:08 -0800

    Add react router dom

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Timeline from './Timeline.js';
+import Landing from './Landing.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <Route exact path="/" component={Landing} />
+            <Route path="/timeline" component={Timeline} />
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ website/src/Timeline.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Timeline extends Component {
+  render() {
+    return (
+      <div className="">
+        <p> Timeline</p>
+      </div>
+    );
+  }
+}
+
+export default Timeline;
