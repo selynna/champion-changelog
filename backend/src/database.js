26eaccce2dfc0492a5845d5b47330f95af64507b
Linnun <github@danielfigge.net>
2018-11-08 15:10:21 -0800

    Update backend/src/database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 
-var debug = false;
+var debug = true;
 var connection = mysql.createConnection({
 	host:'aurora.lolskill.net',
 	user:'hackathon',
@@ -26,10 +26,9 @@ exports.getAllPatches = function(limit){
 		connection.query(`
 			SELECT *
 			FROM patch
-			ORDER BY id DESC
+			ORDER BY date DESC
 			LIMIT ?
 		`, [limit], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -38,6 +37,8 @@ exports.getAllPatches = function(limit){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
@@ -58,7 +59,6 @@ exports.getCurrentPatchForDate = function(date){
 			ORDER BY date DESC
 			LIMIT 1
 		`, [date], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -67,6 +67,8 @@ exports.getCurrentPatchForDate = function(date){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
@@ -82,7 +84,6 @@ exports.getAllChangesForChampionId = function(championId, limit){
 			ORDER BY patchId DESC
 			LIMIT ?
 		`, [championId, limit], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -91,6 +92,8 @@ exports.getAllChangesForChampionId = function(championId, limit){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
@@ -104,7 +107,6 @@ exports.getAllChangesForChampionIdAfterDate = function(championId, date){
 			AND date > ?
 			ORDER BY patchId DESC
 		`, [championId, date], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -113,6 +115,8 @@ exports.getAllChangesForChampionIdAfterDate = function(championId, date){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
@@ -124,7 +128,6 @@ exports.getRelevantItemsForChampionId = function(championId){
 			WHERE championId = ?
 			ORDER BY championId DESC
 		`, [championId], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -133,6 +136,8 @@ exports.getRelevantItemsForChampionId = function(championId){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
@@ -144,7 +149,6 @@ exports.getRelevantRunesForChampionId = function(championId){
 			WHERE championId = ?
 			ORDER BY runeId DESC
 		`, [championId], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -153,6 +157,8 @@ exports.getRelevantRunesForChampionId = function(championId){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
@@ -165,7 +171,6 @@ exports.getItemChangesForPatchIdAndItemId = function(patchId, itemId){
 			AND itemId = ?
 			ORDER BY patchId DESC
 		`, [patchId, itemId], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -174,6 +179,8 @@ exports.getItemChangesForPatchIdAndItemId = function(patchId, itemId){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
@@ -186,7 +193,6 @@ exports.getRuneChangesForPatchIdAndRuneId = function(patchId, runeId){
 			AND runeId = ?
 			ORDER BY patchId DESC
 		`, [patchId, runeId], function(err, rows, fields){
-//			connection.end();
 			if(!err){
 				if(debug) console.log('(DEBUG) result: ', rows);
 				resolve(rows);
@@ -195,61 +201,135 @@ exports.getRuneChangesForPatchIdAndRuneId = function(patchId, runeId){
 				reject();
 			}
 		});
+	}).then(function(){
+//		connection.end();
 	})
 };
 
-exports.getAllItemChangesForPatchIdAndChampionId = function(patchId, championId){
+exports.getAllItemChangesForPatchIdAndChampionId = function(patchId, championId, cb){
+	connection.query(`
+		SELECT *
+		FROM patch_item_changes
+		RIGHT JOIN champion_items ON (champion_items.itemId = patch_item_changes.itemId)
+		WHERE patch_item_changes.patchId = ?
+		AND champion_items.championId = ?
+		ORDER BY champion_items.itemId ASC
+	`, [patchId, championId], function(err, rows, fields){
+		if(!err){
+			if(debug) console.log('(DEBUG) result: ', rows);
+			cb(rows);
+		}else{
+			if(debug) console.log('(DEBUG) db query error: ' + err);
+			cb(null);
+		}
+	});
+};
+
+exports.getAllRuneChangesForPatchIdAndChampionId = function(patchId, championId, cb){
+	connection.query(`
+		SELECT *
+		FROM patch_rune_changes
+		RIGHT JOIN champion_runes ON (champion_runes.runeId = patch_rune_changes.runeId)
+		WHERE patch_rune_changes.patchId = ?
+		AND champion_runes.championId = ?
+		ORDER BY champion_runes.runeId ASC
+	`, [patchId, championId], function(err, rows, fields){
+		if(!err){
+			if(debug) console.log('(DEBUG) result: ', rows);
+			cb(rows);
+		}else{
+			if(debug) console.log('(DEBUG) db query error: ' + err);
+			cb(null);
+		}
+	});
+};
+
+exports.getAllPatchesAfterPatchId = function(patchId, cb){
+	connection.query(`
+		SELECT *
+		FROM patch
+		WHERE date >= (
+			SELECT date
+			FROM patch
+			WHERE id = ?
+		)
+		ORDER BY date DESC
+	`, [patchId], function(err, rows, fields){
+		if(!err){
+			if(debug) console.log('(DEBUG) result: ', rows);
+			cb(rows);
+		}else{
+			if(debug) console.log('(DEBUG) db query error: ' + err);
+			cb(null);
+		}
+	});
+};
+
+exports.getAllItemChangesForChampionIdAfterPatchId = function(championId, patchId){
 	return new Promise((resolve, reject) => {
-		connection.query(`
-			SELECT *
-			FROM patch_item_changes
-			RIGHT JOIN champion_items ON (champion_items.itemId = patch_item_changes.itemId)
-			WHERE patch_item_changes.patchId = ?
-			AND champion_items.championId = ?
-			ORDER BY champion_items.itemId ASC
-		`, [patchId, championId], function(err, rows, fields){
-//			connection.end();
-			if(!err){
-				if(debug) console.log('(DEBUG) result: ', rows);
-				resolve(rows);
-			}else{
-				if(debug) console.log('(DEBUG) db query error: ' + err);
-				reject();
+		var result = {};
+
+		exports.getAllPatchesAfterPatchId(patchId, (rows) => {			
+			if(!rows){
+				return;
 			}
+			
+			let i = 0;
+			rows.forEach(function(row){
+				var patchId = row.id;
+
+				exports.getAllItemChangesForPatchIdAndChampionId(patchId, championId, (itemChanges) => {
+					i++;
+					result[patchId] = itemChanges;
+					if (i==rows.length){
+						resolve(result);
+					}
+				});
+			});
+			
 		});
 	})
 };
 
-exports.getAllRuneChangesForPatchIdAndChampionId = function(patchId, championId){
+exports.getAllRuneChangesForChampionIdAfterPatchId = function(championId, patchId){
 	return new Promise((resolve, reject) => {
-		connection.query(`
-			SELECT *
-			FROM patch_rune_changes
-			RIGHT JOIN champion_runes ON (champion_runes.runeId = patch_rune_changes.runeId)
-			WHERE patch_rune_changes.patchId = ?
-			AND champion_runes.championId = ?
-			ORDER BY champion_runes.runeId ASC
-		`, [patchId, championId], function(err, rows, fields){
-//			connection.end();
-			if(!err){
-				if(debug) console.log('(DEBUG) result: ', rows);
-				resolve(rows);
-			}else{
-				if(debug) console.log('(DEBUG) db query error: ' + err);
-				reject();
+		var result = {};
+
+		exports.getAllPatchesAfterPatchId(patchId, (rows) => {			
+			if(!rows){
+				return;
 			}
+			
+			let i = 0;
+			rows.forEach(function(row){
+				var patchId = row.id;
+
+				exports.getAllRuneChangesForPatchIdAndChampionId(patchId, championId, (runeChanges) => {
+					i++;
+					result[patchId] = runeChanges;
+					if (i==rows.length){
+						resolve(result);						
+					}
+				});
+			});
+			
 		});
 	})
 };
 
 // testing
 if(debug){
-	exports.getPatchForDate(new Date('2018-06-01')).then((response) => {
-		console.log(response);
+//	var x = exports.getAllItemChangesForChampionIdAfterPatchId(104, '8.1');
+//	console.log('x', x);
+	exports.getAllItemChangesForChampionIdAfterPatchId(104, '8.1').then((response) => {
+		console.log('xxx', response);
 	});
-	exports.getAllChangesForChampionIdAfterDate(142, new Date('2018-01-01')).then((response) => {
-		console.log(response);
-	});
+//	exports.getCurrentPatchForDate(new Date('2017-11-08T21:33:49.987Z')).then((response) => {
+//		console.log(response);
+//	});
+//	exports.getAllChangesForChampionIdAfterDate(142, new Date('2018-01-01')).then((response) => {
+//		console.log(response);
+//	});
 //	exports.getAllItemChangesForPatchIdAndChampionId('8.22', 1).then((response) => {
 //		console.log(response);
 //	});
