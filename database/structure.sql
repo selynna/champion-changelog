2cddc49f2be7f7ae356e21c2c4a0f50d8359090d
Linnun <github@danielfigge.net>
2018-11-07 16:26:00 -0800

    Update database/structure.sql
    - made patch.id varchar(255), before was int(10)
    - dropped patch.name
    
    patch.id will now simply be a string like  "8.22"

--- database/structure.sql
+++ database/structure.sql
@@ -9,8 +9,7 @@
 --
 
 CREATE TABLE `patch` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `id` varchar(255) NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -21,7 +20,7 @@ CREATE TABLE `patch` (
 --
 
 CREATE TABLE `patch_champion_changes` (
-  `patchId` int(10) UNSIGNED NOT NULL,
+  `patchId` varchar(255) NOT NULL,
   `championId` int(10) UNSIGNED NOT NULL,
   `changes` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -33,7 +32,7 @@ CREATE TABLE `patch_champion_changes` (
 --
 
 CREATE TABLE `patch_champion_items` (
-  `patchId` int(10) UNSIGNED NOT NULL,
+  `patchId` varchar(255) NOT NULL,
   `championId` int(10) UNSIGNED NOT NULL,
   `itemId` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -45,7 +44,7 @@ CREATE TABLE `patch_champion_items` (
 --
 
 CREATE TABLE `patch_champion_runes` (
-  `patchId` int(10) UNSIGNED NOT NULL,
+  `patchId` varchar(255) NOT NULL,
   `championId` int(10) UNSIGNED NOT NULL,
   `runeId` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -57,7 +56,7 @@ CREATE TABLE `patch_champion_runes` (
 --
 
 CREATE TABLE `patch_item_changes` (
-  `patchId` int(10) UNSIGNED NOT NULL,
+  `patchId` varchar(255) NOT NULL,
   `itemId` int(10) UNSIGNED NOT NULL,
   `changes` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -69,7 +68,7 @@ CREATE TABLE `patch_item_changes` (
 --
 
 CREATE TABLE `patch_rune_changes` (
-  `patchId` int(10) UNSIGNED NOT NULL,
+  `patchId` varchar(255) NOT NULL,
   `runeId` int(10) UNSIGNED NOT NULL,
   `changes` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
