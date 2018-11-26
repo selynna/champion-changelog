29927590887b8f58563d1e1f731b7f5dbffd2dc0
Linnun <github@danielfigge.net>
2018-11-26 23:04:19 +0100

    Delete config.js

--- backend/src/database/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	connectionLimit:50,
-	host:'aurora.lolskill.net',
-	port:3306,
-	user:'championlog',
-	password:'koBbhCpmmRcfNC4a',
-	database:'championlog',
-	charset:'UTF8_GENERAL_CI',
-};
